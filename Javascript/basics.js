aeea371060f2b331813ed0763da3a833b10a8dd0
Asif Khan <asifkhan882609@gmail.com>
2025-12-02 01:36:20 +0530

    CSS_higher

---
+++ Javascript/basics.js
@@ -0,0 +1,14 @@
+console.log("hello")            // its just like string i mean simple text
+console.warn("hello warning")   //its a warning 
+console.error("hello error")     // its a error
+
+
+//alert("hello User")            // its a notification type alert on web
+var nam = prompt("What's Your name")     // its same like alert but interactive mode like we can use to ask you somethingand use anywhere
+console.log(nam)  // this show my name whats i give to the prompt , in console
+
+
+// Variable and Constants
+
+var a = 50;      //Variable can be anytime anywhere
+const b = 30;     //Constant is fixed you set one time , you cannot be update it anymore
